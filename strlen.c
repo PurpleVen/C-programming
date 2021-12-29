aefbda895e8ba9584b4386e7057a5c8ef28d7bb1
S.Vendra <86104620+PurpleVen@users.noreply.github.com>
2021-12-29 11:47:36 +0530

    Add files via upload

---
+++ strlen.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main ( )
+{
+char x[10];
+printf("enter a strings to x");
+scanf("%s",&x);
+printf("\n x string length is =%d",strlen(x));
+getch( );
+}
+
