f81b6ac45c4508147def0a9fc09d3a57775b29dd
S.Vendra <86104620+PurpleVen@users.noreply.github.com>
2022-02-07 20:18:33 +0530

    Add files via upload

---
+++ Prog to print nos from 1 to 10 using while loop.c
@@ -0,0 +1,15 @@
+// Prog to print nos from 1 to 10 using while loop
+#include<stdio.h>
+#include<conio.h>
+
+int  main()
+{
+	int a = 1;
+	while(a<=10)
+	{
+		printf("%d \n", a);
+		a++;
+	}
+	getch();
+	return 0;
+}
