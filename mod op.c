7e58a796ce5823ec7d54e078f206fe8135dac970
S.Vendra <86104620+PurpleVen@users.noreply.github.com>
2022-02-09 09:12:36 +0530

    Add files via upload

---
+++ mod op.c
@@ -0,0 +1,17 @@
+  #include<stdio.h>
+  int main()
+  {
+  	int a=10;
+  	int b=15;
+  	int c;
+  	
+  	//modulus operator generates the remainder
+  	
+  	c = a % b;
+  	
+  	//print the result
+  	
+  	printf("Result in variable c is %d \n",c);
+  	
+  	return 0;
+  }
