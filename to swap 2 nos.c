4b5edf9d30f3d546cfbbd6a5202c91e4138051e4
S.Vendra <86104620+PurpleVen@users.noreply.github.com>
2022-03-12 20:06:30 +0530

    Add files via upload

---
+++ to swap 2 nos.c
@@ -0,0 +1,15 @@
+// WAP to swap 2 nos
+
+//a      b    temp
+//5      6     0
+
+//temp   <- a    //a= b= c=
+
+temp=a ; // a = 5  b = 6  temp = 5
+a=b;     // a = 6  b = 6  temp = 5
+b=temp;  // a = 6  b = 5  temp = 5
+
+
+#include<stdio.h>
+#include<conio.h> 
+      
