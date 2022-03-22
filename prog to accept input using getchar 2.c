dde8989b92623ee5200fd98b11d73aa67d2b95d5
S.Vendra <86104620+PurpleVen@users.noreply.github.com>
2022-03-22 23:30:45 +0530

    Add files via upload

---
+++ prog to accept input using getchar 2.c
@@ -0,0 +1,16 @@
+// prog to accept input using getchar
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+	char s[10] = "DBIT"; int i; char ch;;
+	for(i=10;i<10;i++)
+	{
+		putchar (s[i]);
+	}
+	for(i=0;i<strlen(s);i++)
+	putchar (s[i]);
+	
+	getch();
+}
