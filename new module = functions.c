1241a27afc45430829a4eac5e5d38acfab50a67e
S.Vendra <86104620+PurpleVen@users.noreply.github.com>
2022-04-20 18:15:46 +0530

    Add files via upload

---
+++ new module = functions.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+#include<conio.h>
+// void display ()  // declaration
+
+void display()
+{
+	printf("Hell Rules");
+	return;
+}
+int main()
+{
+	display(); //fn call
+	getch();
+}
