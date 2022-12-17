37e9b84cdeb609c1a0f3ffa065e8ae6c08ee6195
S.Vendra <86104620+PurpleVen@users.noreply.github.com>
2022-12-18 00:22:19 +0530

    Add files via upload

---
+++ crossword puzzle.c
@@ -0,0 +1,105 @@
+ //#include void main() { int k = 0; for (k) printf("Hello"); }
+ 
+ // #include void main(){ int a=10; if(printf(“%d”,a>=10)-10) for(;;) break; else; } 
+ 
+/* #include<stdio.h> 
+ main()
+  { int ch; 
+  printf("enter a value btw 1 to 2:"); 
+  scanf("%d", &ch); 
+  switch (ch, ch + 1) 
+  { case 1: 
+  printf("1"); 
+  break; 
+  case 2: printf("2");
+   break; }
+  }
+  */
+  
+ /* #include<stdio.h>
+  int main()
+  { int x=11,i; 
+  for(i=0;i
+  */
+  
+  /*#include<stdio.h>
+  void main()
+   {int k;
+    for (k = -3; k < -5; k++) 
+	printf("Hello");
+	 }
+	 */
+	 
+/*	 #include<stdio.h>
+ void main()
+
+{
+
+double k = 0;
+
+for (k = 0.0; k < 3.0; k++)
+
+printf("Hello"); }
+*/
+
+/*#include<stdio.h>
+main()
+ { 
+ int i;
+  for(i=0;i<10;i++);
+   printf("%d ",i); }
+   */
+   
+  /* #include<stdio.h>
+ int main()
+
+{
+
+short i;
+
+for (i = 1; i >= 0; i++)
+
+printf("%d ", i); }
+*/
+
+/* #include<stdio.h>
+ int main() 
+ {
+  int i=1; 
+  for(;;)
+  {
+   printf("%d", i++); 
+   if(i>10) break; 
+   } 
+   return 0;
+}
+*/
+
+/*#include<stdio.h>
+void main() 
+{ 
+int i;
+ for(i=0;i<10;i++);
+  printf("%d ",i);
+   } 
+   */
+   
+  /* #include<stdio.h>
+   void main() 
+   { int x = 5;
+    if (true); 
+	printf("hello");
+	 }
+	 */
+	 
+/*	 #include<stdio.h>
+ int main()
+
+{
+
+short i;
+
+for (i = 1; i >= 0; i++)
+
+printf("%d ", i); }
+*/
